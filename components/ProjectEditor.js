ded1a1c9b54f195963d65f94ccd65a253c1eb489
Étienne Després <etienne@pochour.com>
2019-03-31 22:14:12 -0400

    Use React hooks

--- components/ProjectEditor.js
+++ components/ProjectEditor.js
@@ -1,83 +1,78 @@
-class ProjectEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: props.name,
-      color: props.color,
-    };
+import { useState } from 'react';
+
+function ProjectEditor(props) {
+  const [name, setName] = useState(props.name);
+  const [color, setColor] = useState(props.color);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
   }
 
-  handleNameChange(event) {
-    this.setState({ name: event.target.value });
-  }
-
-  handleColorChange(event) {
-    this.setState({ color: event.target.value });
+  function handleColorChange(event) {
+    setColor(event.target.value);
   }
   
-  render() {
-    return (
-      <div>
-        <section>
-          <label htmlFor="name">Nom du projet</label>
-          <input type="text" id="name" value={this.state.name} onChange={this.handleNameChange.bind(this)} />
-          <label htmlFor="color">Couleur</label>
-          <input type="text" id="color" value={this.state.color} onChange={this.handleColorChange.bind(this)} />
-          <button onClick={this.props.update.bind(this, this.state.name, this.state.color)}>Enregistrer</button>
-        </section>
-        <style jsx>{`
-          div {
-            position: absolute;
-            background-color: rgba(0, 0, 0, 0.2);
-            top: 0;
-            bottom: 0;
-            left: 0;
-            right: 0;
-            z-index: 2;
-          }
+  return (
+    <div>
+      <section>
+        <label htmlFor="name">Nom du projet</label>
+        <input type="text" id="name" value={name} onChange={handleNameChange} />
+        <label htmlFor="color">Couleur</label>
+        <input type="text" id="color" value={color} onChange={handleColorChange} />
+        <button onClick={props.update.bind(this, name, color)}>Enregistrer</button>
+      </section>
+      <style jsx>{`
+        div {
+          position: absolute;
+          background-color: rgba(0, 0, 0, 0.2);
+          top: 0;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          z-index: 2;
+        }
+      
+        section {
+          position: absolute;
+          display: flex;
+          flex-direction: column;
+          width: 90%;
+          margin: auto;
+          padding: 2em;
+          top: 45%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          background-color: white;
+          box-shadow: 0 10px 30px 5px rgba(0, 0, 0, 0.2);
+        }
+      
+        label {
+          font-size: 0.9em;
+          font-weight: bold;
+          width: 100%;
+          padding-bottom: 0.8em;
+        }
+      
+        input {
+          margin-bottom: 1em;
+        }
+      
+        button {
+          cursor: pointer;
+          margin-top: 1em;
+          background-color: #0B1099;
+          color: white;
+          border: 1px solid #0B1099;
+        }
         
+        @media (min-width: 768px) {
           section {
-            position: absolute;
-            display: flex;
-            flex-direction: column;
-            width: 90%;
-            margin: auto;
-            padding: 2em;
-            top: 45%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            background-color: white;
-            box-shadow: 0 10px 30px 5px rgba(0, 0, 0, 0.2);
+            width: 320px;
           }
-        
-          label {
-            font-size: 0.9em;
-            font-weight: bold;
-            width: 100%;
-            padding-bottom: 0.8em;
-          }
-        
-          input {
-            margin-bottom: 1em;
-          }
-        
-          button {
-            cursor: pointer;
-            margin-top: 1em;
-            background-color: #0B1099;
-            color: white;
-            border: 1px solid #0B1099;
-          }
-          
-          @media (min-width: 768px) {
-            section {
-              width: 320px;
-            }
-          }
-        `}</style>
-      </div>
-    );
-  }
+        }
+      `}</style>
+    </div>
+  );
 }
 
 export default ProjectEditor;
--- components/Week.js
+++ components/Week.js
@@ -1,100 +1,93 @@
-import React from 'react';
+import { useState } from 'react';
 import getConfig from 'next/config';
 import Hour from '../components/Hour';
 
 const { publicRuntimeConfig } = getConfig()
 
-class Week extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPainting: false,
+function Week({ assignProjectToHour, weekId, offset, date, hours, getProject }) {
+  const [isPainting, setIsPainting] = useState(false);
+  
+  function mouseEnterHour(hourId) {
+    if (isPainting) {
+      assignProjectToHour(weekId, hourId);
     }
   }
   
-  mouseEnterHour(hourId) {
-    if (this.state.isPainting) {
-      this.props.assignProjectToHour(this.props.weekId, hourId);
-    }
+  function mouseDownHour(hourId) {
+    setIsPainting(true);
+    assignProjectToHour(weekId, hourId);
   }
   
-  mouseDownHour(hourId) {
-    this.setState({ isPainting: true });
-    this.props.assignProjectToHour(this.props.weekId, hourId);
+  function mouseUpHour(hourId) {
+    setIsPainting(false);
   }
   
-  mouseUpHour(hourId) {
-    this.setState({ isPainting: false });
-  }
+  const weekOf = date.toLocaleDateString('fr-CA', { day: 'numeric', month: 'long' });
+  const hoursPerRow = Math.ceil(Math.sqrt(publicRuntimeConfig.hoursPerWeek)) + 1;
+  const labelledProjects = {};
+  const hoursByProject = {};
   
-  render() {
-    const { offset, date, hours, weekId, getProject } = this.props;
-    const weekOf = date.toLocaleDateString('fr-CA', { day: 'numeric', month: 'long' });
-    const hoursPerRow = Math.ceil(Math.sqrt(publicRuntimeConfig.hoursPerWeek)) + 1;
-    const labelledProjects = {};
-    const hoursByProject = {};
-    
-    if (hours) {
-      for (const hour in hours) {
-        if (hours.hasOwnProperty(hour) && hours[hour]) {
-          const projectId = hours[hour];
-          const currentHourCount = hoursByProject[projectId];
-          hoursByProject[projectId] = currentHourCount ? currentHourCount + 1 : 1;
-        }
+  if (hours) {
+    for (const hour in hours) {
+      if (hours.hasOwnProperty(hour) && hours[hour]) {
+        const projectId = hours[hour];
+        const currentHourCount = hoursByProject[projectId];
+        hoursByProject[projectId] = currentHourCount ? currentHourCount + 1 : 1;
       }
     }
-      
-    return (
-      <section>
-        <h2>{ offset === 0 ? 'Cette semaine' : `Semaine du ${weekOf}`}</h2>
-        <ul>
-          { [...Array(publicRuntimeConfig.hoursPerWeek)].map((x, i) => {
-            const projectId = hours ? hours[i] : undefined;
-            let showLabel = false;
-            
-            if (projectId) {
-              showLabel = labelledProjects[projectId] ? false : true;
-              labelledProjects[projectId] = true;
-            }
-            
-            return (
-              <li key={i}>
-                <Hour
-                  mouseDown={this.mouseDownHour.bind(this)}
-                  mouseEnter={this.mouseEnterHour.bind(this)}
-                  mouseUp={this.mouseUpHour.bind(this)}
-                  weekId={weekId}
-                  hourId={i}
-                  projectId={projectId}
-                  count={hoursByProject[projectId]}
-                  getProject={getProject}
-                  showLabel={showLabel}
-                />
-              </li>
-            );
-          }) }
-        </ul>
-        <style jsx>{`
-          section {
-            margin-bottom: 1.5em;
-          }
-          
-          h2 {
-            font-size: 1em;
-          }
-          
-          ul {
-            display: flex;
-            flex-wrap: wrap;
-          }
-
-          li {
-            width: ${100 / hoursPerRow}%;
-          }
-        `}</style>
-      </section>
-    );
   }
+      
+  return (
+    <section>
+      <h2>{ offset === 0 ? 'Cette semaine' : `Semaine du ${weekOf}`}</h2>
+      <ul>
+        { [...Array(publicRuntimeConfig.hoursPerWeek)].map((x, i) => {
+          const projectId = hours ? hours[i] : undefined;
+          let showLabel = false;
+          
+          if (projectId) {
+            showLabel = labelledProjects[projectId] ? false : true;
+            labelledProjects[projectId] = true;
+          }
+          
+          return (
+            <li key={i}>
+              <Hour
+                mouseDown={mouseDownHour}
+                mouseEnter={mouseEnterHour}
+                mouseUp={mouseUpHour}
+                weekId={weekId}
+                hourId={i}
+                projectId={projectId}
+                count={hoursByProject[projectId]}
+                getProject={getProject}
+                showLabel={showLabel}
+              />
+            </li>
+          );
+        }) }
+      </ul>
+      <style jsx>{`
+        section {
+          margin-bottom: 1.5em;
+          user-select: none;
+        }
+        
+        h2 {
+          font-size: 1em;
+        }
+        
+        ul {
+          display: flex;
+          flex-wrap: wrap;
+        }
+
+        li {
+          width: ${100 / hoursPerRow}%;
+        }
+      `}</style>
+    </section>
+  );
 }
 
 export default Week;
