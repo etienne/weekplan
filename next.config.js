df9b2f3049274112d4741eec7130e32fc3b76aeb
Étienne Després <etienne@pochour.com>
2019-12-20 11:22:25 -0500

    Increase visible weeks

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicRuntimeConfig: {
     serverEndpoint: process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : '/api/',
     hoursPerWeek: 26,
-    visibleWeeks: 12,
+    visibleWeeks: 18,
     colors: [
       '#A7F3F0',
       '#0B1099',
